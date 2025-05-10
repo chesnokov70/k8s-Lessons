aab478f9758d0684a562dc3c178b3aa7cf32e32b
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-05-10 16:27:04 -0400

    added Les-9

--- Lesson-06-DockerImage/index.php
+++ Lesson-06-DockerImage/index.php
@@ -3,5 +3,5 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello From Kubernetes</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <font color=blue>Denis Astahov";
+echo "Made by <font color=blue>Sergei Chesnokov";
 ?>
